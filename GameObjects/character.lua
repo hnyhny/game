46ce9d3a66a5f29ac2efd4bc0a1144c1e88e789d
Hein I <not@provid.ed>
2021-02-12 01:49:25 +0100

    Player kann nur noch einmal springen

--- GameObjects/character.lua
+++ GameObjects/character.lua
@@ -7,15 +7,16 @@ local modelWidth = images.character:getWidth()
 local modelHeight = images.character:getHeight()
 
 local level = settings.Screen.Virtual
-local gameConfig = settings.Game
+local characterConfig = settings.Game.Character
+
 local Level = {
   Boarders = {
-  Left = 0.5 * modelWidth,
-  Right = level.Width - 1.5 * modelWidth,
-  Bottom = level.Height - 1.5 * modelHeight
+    Left = 0.5 * modelWidth,
+    Right = level.Width - 1.5 * modelWidth,
+    Bottom = level.Height - 1.5 * modelHeight
   },
   Start = {
-    X = (level.Width -modelWidth) / 2,
+    X = (level.Width - modelWidth) / 2,
     Y = level.Height
   }
 }
@@ -31,37 +32,42 @@ function Character:render()
   love.graphics.draw(images.character, self.x, self.y)
 end
 
+local Jumped = {
+  Once = false,
+}
 
 local function updateYVelocity(yVelocity, deltaTime)
-  if userInput.isJump() then
-    return -gameConfig.Character.Jump
+  if userInput.isJump() and not Jumped.Once then
+    Jumped.Once = true
+    return -characterConfig.Jump.First
+ 
   else
-    return yVelocity + gameConfig.Gravity * deltaTime
+    return yVelocity + settings.Game.Gravity * deltaTime
   end
 end
 
 local function moveY(y, yVelocity)
   local newY = y + yVelocity
-  
+
   if newY > Level.Boarders.Bottom then
+    Jumped.Once = false
     return Level.Boarders.Bottom
   else
     return newY
   end
 end
 
-
 local function moveX(x)
   local newX = x
   if userInput.isMoveRight() then
-    newX = newX + gameConfig.Character.Movement
+    newX = newX + characterConfig.Movement
   elseif userInput.isMoveLeft() then
-    newX = newX - gameConfig.Character.Movement
+    newX = newX - characterConfig.Movement
   end
   if newX < Level.Boarders.Left then
     return Level.Boarders.Left
   end
-  
+
   if newX > Level.Boarders.Right then
     return Level.Boarders.Right
   end
